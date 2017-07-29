d02801d018dd6616eeeb2f38d829aa3ba633f388
SATYA <satyapriyarajput@gmail.com>
2017-07-29 19:22:07 +0530

    Second

--- app/src/main/java/com/satya/mywork/mywork/MainActivity.java
+++ app/src/main/java/com/satya/mywork/mywork/MainActivity.java
@@ -2,12 +2,41 @@ package com.satya.mywork.mywork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    View loginView,signupView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginView = (View)findViewById(R.id.loginView);
+        loginView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                loginView.setBackgroundResource(R.color.selectedTab);
+                signupView.setBackgroundResource(R.color.deSelectedTab);
+                return true;
+            }
+        });
+
+        signupView = (View)findViewById(R.id.signupView);
+        signupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                loginView.setBackgroundResource(R.color.deSelectedTab);
+                v.setBackgroundResource(R.color.selectedTab);
+                return true;
+            }
+        });
+
     }
+
+
 }
