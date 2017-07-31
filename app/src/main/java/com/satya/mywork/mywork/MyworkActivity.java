1e97dc40980d4f481dd06952ebc78b315ec08287
apple <satyapriya.arch@gmail.com>
2017-07-31 17:31:09 +0530

    today

--- app/src/main/java/com/satya/mywork/mywork/MyworkActivity.java
+++ app/src/main/java/com/satya/mywork/mywork/MyworkActivity.java
@@ -2,12 +2,32 @@ package com.satya.mywork.mywork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MyworkActivity extends AppCompatActivity {
 
+
+    Button addworkButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mywork);
+
+        addworkButton = (Button)findViewById(R.id.addworkButton);
+        addworkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("Satya","ButtonTouched");
+
+
+
+
+            }
+        });
+
+
     }
 }
